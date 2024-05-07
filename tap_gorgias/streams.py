1f8b75b3aa77c666048a7dbd12f966d0fdb7f9af
JesuFemi-O <ogunwedeemmanuel@gmail.com>
2024-05-07 13:45:35 +0100

    set is_sorted to false

--- tap_gorgias/streams.py
+++ tap_gorgias/streams.py
@@ -31,7 +31,7 @@ class TicketsStream(GorgiasStream):
     path = "/api/views/{view_id}/items"
     primary_keys = ["id"]
     replication_key = "updated_datetime"
-    is_sorted = True
+    # is_sorted = True
 
     # Link to the next items, if any.
     next_page_token_jsonpath = "$.meta.next_items"
